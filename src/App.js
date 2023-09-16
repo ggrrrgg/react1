81b892356d159193de5a394414917adc5d05b850
ggrrrgg <george.sheridan@gmail.com>
2023-09-16 11:47:42 +1000

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+// import logo from './logo.svg';
+import './App.css';
+
+import PokemonDisplay from './PokemonDisplay';
+
+function App() {
+  return (
+    <div className="App">
+      {/* pokemondisplay is an instance of the component */}
+      <PokemonDisplay/>
+    </div>
+  );
+}
+
+export default App;
